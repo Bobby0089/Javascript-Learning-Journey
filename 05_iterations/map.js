a0a3f6ad263625070ce3deda4d7a79db642a69ef
Bobby Gupta <161326642+Bobby0089@users.noreply.github.com>
2025-11-26 04:19:16 +0000

    Add JS Iteration example: map()

---
+++ 05_iterations/map.js
@@ -0,0 +1,19 @@
+/*  MAP()?
+
+map() is an array method that:
+Loops through every item
+Transforms/modifies each item
+Returns a NEW array with the transformed values
+Does NOT change the original array
+*/
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
